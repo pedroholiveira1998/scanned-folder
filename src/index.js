f6b2f998e69620f6412d5f97a9ca3d6c85439ab0
Pedro Henrique <pdr_castro@hotmail.com>
2021-01-26 08:05:48 -0300

    initial setup, connect node with mongodb

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || '8000';
+
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+mongoose.connect('mongodb+srv://root:123@cluster0.qv3nf.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+app.listen(port, function () {
+    console.log('Web server listening on port ' + port);
+});
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose connected');
+});
